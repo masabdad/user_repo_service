de81cdbf8b849f2379c77a5cc0a404779c74b38a
tassawarabbas <tasawar.javadeveloper@gmail.com>
2025-04-01 21:28:38 +0500

    Initial commit

---
+++ src/main/java/com/mysample/Sample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.mysample.Sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
